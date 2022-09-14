00722c31d5bebffb58b8343a781b7a8ddb905b8a
ImmortalDante <maklakovfdr@gmail.com>
2022-09-14 16:54:50 +0300

    Authentication functions added

--- students/views.py
+++ students/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import *
 
 
 def user_login(request):
-    return render(request, 'auth/login.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('signup')
+    else:
+        return render(request, 'auth/login.html', {})
 
 
 def signup(request):
-    return render(request, 'auth/signup.html', {})
+    if request.method == 'POST':
+        form = RegUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('signup')
+    else:
+        form = RegUserForm()
+        return render(request, 'auth/signup.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
