73e6c90ebbe22f6d2bb59ac982ddea16cc09873d
ImmortalDante <maklakovfdr@gmail.com>
2022-09-14 14:53:09 +0300

    UserOptional model and registration forms added

---
+++ auth/forms.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import *
+from django.forms import ModelForm
+
+
+class UserOptionalForm(ModelForm):
+    class Meta:
+        model = UserOptional
+        fields = ('faculty', 'specialty', 'group', 'description', 'tg_link')
+        labels = {
+            'faculty': 'Факультет',
+            'specialty': 'Специальность',
+            'group': 'Группа',
+            'description': 'Описание',
+            'tg_link': 'Ссылка на телеграм',
+        }
+        widgets = {
+            'faculty': forms.Select(
+                attrs={'class': 'form-control'},
+                choices=[
+                    ('fee', 'ИЭФ'),
+                    ('fcad', 'ФКП'),
+                    ('fitc', 'ФИТУ'),
+                    ('fre', 'ФРЭ'),
+                    ('fcsn', 'КСиС'),
+                    ('mf', 'ВФ'),
+                    ()
+                ])
+        }
+
+
+class RegUserForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(RegUserForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class UserOptional(models.Model):
+    faculty = models.CharField('Факультет', max_length=5)
+    specialty = models.CharField('Специальность', max_length=255)
+    group = models.CharField('Номер группы', max_length=6)
+    description = models.TextField('Описание', max_length=1000)
+    tg_link = models.URLField('Ссылка на телеграм')
+    user = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
