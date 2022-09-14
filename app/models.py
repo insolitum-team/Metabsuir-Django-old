8248656fcb43b821b69a2b80ea19e83d236720a3
esquiresword <71277778+esquiresword@users.noreply.github.com>
2022-09-14 04:49:57 -0700

    section, theme and message models added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Section(models.Model):
+    name = models.CharField('Название секции', max_length=255)
+
+class Theme(models.Model):
+    name = models.CharField('Название темы', max_length=255)
+    date = models.DateTimeField('Дата создания')
+    main_post = models.TextField('Главный пост')
+    author = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, blank=False, null=False, on_delete=models.CASCADE)
+
+class Message(models.Model):
+    content = models.TextField('Содержание')
+    theme = models.ForeignKey(Theme, blank=False, null=False, on_delete=models.CASCADE)
+    reply_to = models.IntegerField('Ответ на')
+    sender = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
+    date = models.DateTimeField('Дата отправки')
+
