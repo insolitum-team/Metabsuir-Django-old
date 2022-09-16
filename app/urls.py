c6b5d7092ebcbdbb27a350d1bedef89386c33b5b
ImmortalDante <maklakovfdr@gmail.com>
2022-09-16 22:01:16 +0300

    Checking themes by section id added

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('themes/<section_id>', theme_by_section, name='theme_by_section'),
 ]
